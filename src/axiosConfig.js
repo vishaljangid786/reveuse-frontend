0d3c486402a20e01b3d178dce2abd2a47a376e2e
vishaljangid786 <vishaljangid80550786@gmail.com>
2025-04-20 09:08:21 +0530

    com

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: "https://the-reveuse-backend.vercel.app/",
   withCredentials: true,
   headers: {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
   },
 });
 
