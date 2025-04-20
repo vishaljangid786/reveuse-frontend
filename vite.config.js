3b3a61a2efb26fbe51509c7b5a6cfc00e6601934
vishaljangid786 <vishaljangid80550786@gmail.com>
2025-04-20 13:51:15 +0530

    commited

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,19 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ["react", "react-dom", "react-router-dom"],
+        },
+      },
+    },
+  },
+  server: {
+    port: 5173,
+  },
 });
